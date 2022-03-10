c7d363e80c3741cd8a18ae3b35e404789ae1d2ee
Joel Maciel <soldado25461@gmail.com>
2022-03-10 19:24:06 -0300

    Project Backend created

---
+++ backend/src/main/java/com/joel/joel/JoelApplication.java
@@ -0,0 +1,13 @@
+package com.joel.joel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoelApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/joel/joel/JoelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.joel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
