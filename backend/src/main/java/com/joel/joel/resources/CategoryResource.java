6c41148a60869e5545a41c95a29cfaad75c24f99
Joel Maciel <soldado25461@gmail.com>
2022-03-15 21:33:06 -0300

    DTO de Category created

--- backend/src/main/java/com/joel/joel/resources/CategoryResource.java
+++ backend/src/main/java/com/joel/joel/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.joel.joel.entities.Category;
+import com.joel.joel.dto.CategoryDTO;
 import com.joel.joel.service.CategoryService;
 
 @RestController
@@ -19,13 +19,11 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
-	
-	
 
 }
 
