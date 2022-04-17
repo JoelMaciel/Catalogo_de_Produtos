f6edcc5c790fd67693b50a49e0ce68ebdbb27a73
Joel Maciel <soldado25461@gmail.com>
2022-04-17 10:29:38 -0300

    buscando por ID

--- backend/src/main/java/com/joel/joel/service/CategoryService.java
+++ backend/src/main/java/com/joel/joel/service/CategoryService.java
@@ -1,6 +1,8 @@
+
 package com.joel.joel.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,13 @@ public class CategoryService {
 
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 
 }
